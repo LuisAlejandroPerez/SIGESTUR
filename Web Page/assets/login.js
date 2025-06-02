4d072d27011eb8929ca88287e92f4ed59b519111
Luis Alejandro Perez <luisalejandro.p.r@outlook.com>
2025-06-02 19:53:38 -0400

    Se agrego el SDK de Firebase para integrar con Authentication

--- Web Page/assets/login.js
+++ Web Page/assets/login.js
@@ -1,15 +1,34 @@
+// Firebase config
+const firebaseConfig = {
+  apiKey: 'AIzaSyC7rGR0OTIRZ_QQc3RGZ1HB88FhqudyFV0',
+  authDomain: 'sigestur-tx.firebaseapp.com',
+  databaseURL: 'https://sigestur-tx-default-rtdb.firebaseio.com',
+  projectId: 'sigestur-tx',
+  storageBucket: 'sigestur-tx.firebasestorage.app',
+  messagingSenderId: '53209086687',
+  appId: '1:53209086687:web:7f55fbc6325b99346b076a',
+};
+
+// Inizializacion de Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+
 const loginForm = document.getElementById('login-form');
-const loginButton = document.getElementById('login-form-submit');
 const loginErrorMsg = document.getElementById('login-error-msg');
 
-loginButton.addEventListener('click', (e) => {
+loginForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  const username = loginForm.username.value;
+  const email = loginForm.username.value;
   const password = loginForm.password.value;
 
-  if (username === 'user' && password === 'web_dev') {
-    window.location.href = 'pages/dashboard.html';
-  } else {
-    loginErrorMsg.style.display = 'block';
-  }
+  auth
+    .signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Login exitoso
+      window.location.href = 'pages/dashboard.html';
+    })
+    .catch((error) => {
+      console.error(error.code, error.message);
+      loginErrorMsg.style.opacity = 1;
+    });
 });
