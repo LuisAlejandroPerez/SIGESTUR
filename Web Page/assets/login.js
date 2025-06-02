1ae7ea2ece6b2d984c64c8d97eaa9b2898dea37f
Luis Alejandro Perez <luisalejandro.p.r@outlook.com>
2025-06-02 19:14:52 -0400

    Se agrego logica luego de un login exitoso

--- Web Page/assets/login.js
+++ Web Page/assets/login.js
@@ -8,8 +8,7 @@ loginButton.addEventListener('click', (e) => {
   const password = loginForm.password.value;
 
   if (username === 'user' && password === 'web_dev') {
-    alert('You have successfully logged in.');
-    location.reload();
+    window.location.href = 'pages/dashboard.html';
   } else {
     loginErrorMsg.style.display = 'block';
   }
